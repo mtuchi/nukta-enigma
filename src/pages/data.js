71362656c2af2713eb2039febe6d800833a6176a
KhadijaMahanga <khadijamahanga@y7mail.com>
2020-04-09 13:36:30 +0300

    remove index.html file

--- src/pages/data.js
+++ src/pages/data.js
@@ -14,27 +14,50 @@ function Data({ popularPosts }) {
         <div className="container">
           <div className="row">
             <div className="col-md-12 col-lg-8">
-                <Title name="Data" />
-                <ul className="nav nav-tabs list-a-pr-30">
-                <li className="active"><a data-toggle="tab" href="#home">Weather</a></li>
-                <li><a data-toggle="tab" href="#menu1">Dar-es-salaam Sock Exchange</a></li>
-                <li><a data-toggle="tab" href="#menu2">Food Commodities</a></li>
-                </ul>
+              <Title name="Data" />
+              <ul className="nav nav-tabs list-a-pr-30">
+                <li className="active">
+                  <a data-toggle="tab" href="#home">
+                    Weather
+                  </a>
+                </li>
+                <li>
+                  <a data-toggle="tab" href="#menu1">
+                    Dar-es-salaam Sock Exchange
+                  </a>
+                </li>
+                <li>
+                  <a data-toggle="tab" href="#menu2">
+                    Food Commodities
+                  </a>
+                </li>
+              </ul>
 
-                <div className="tab-content">
+              <div className="tab-content">
                 <div id="home" className="tab-pane fade in active">
-                    <h3>HOME</h3>
-                    <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.</p>
+                  <h3>HOME</h3>
+                  <p>
+                    Lorem ipsum dolor sit amet, consectetur adipisicing elit,
+                    sed do eiusmod tempor incididunt ut labore et dolore magna
+                    aliqua.
+                  </p>
                 </div>
                 <div id="menu1" className="tab-pane fade">
-                    <h3>Menu 1</h3>
-                    <p>Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.</p>
+                  <h3>Menu 1</h3>
+                  <p>
+                    Ut enim ad minim veniam, quis nostrud exercitation ullamco
+                    laboris nisi ut aliquip ex ea commodo consequat.
+                  </p>
                 </div>
                 <div id="menu2" className="tab-pane fade">
-                    <h3>Menu 2</h3>
-                    <p>Sed ut perspiciatis unde omnis iste natus error sit voluptatem accusantium doloremque laudantium, totam rem aperiam.</p>
+                  <h3>Menu 2</h3>
+                  <p>
+                    Sed ut perspiciatis unde omnis iste natus error sit
+                    voluptatem accusantium doloremque laudantium, totam rem
+                    aperiam.
+                  </p>
                 </div>
-                </div>
+              </div>
             </div>
             <div className="col-md-6 col-lg-4">
               <div className="pl-20 pl-md-0">
